f90a4c0222609b4fbe3b693980e02fa4daa69b5b
hpolak <hpolak@mion.elka.pw.edu.pl>
2019-01-19 23:38:53 +0100

    unification

--- Klasy.py
+++ Klasy.py
@@ -5,6 +5,11 @@ class Argument:
     def __str__(self):
         return self.Nazwa
 
+    def stala(self):
+        if not self.Nazwa[0].isupper():
+            return False
+        return True
+
     __repr__ = __str__  # dont do that often, repr should return Argument(nazwa) rather than nazwa itself..
 
 
@@ -12,6 +17,20 @@ class Predykat:
     def __init__(self, nazwa, argumenty):
         self.Nazwa = nazwa
         self.Argumenty = argumenty
+
+    def rezolucjowalny(self, inny_predykat):
+        if not (self.Nazwa == inny_predykat.Nazwa):
+            return False
+        if len(self.Argumenty) != len(inny_predykat.Argumenty):
+            return False
+        if self.Argumenty[0].Nazwa == '':
+            return True
+
+        for index, argument in enumerate(self.Argumenty):
+            if argument.stala() and inny_predykat.Argumenty[index].stala():
+                if argument.Nazwa != inny_predykat.Argumenty[index].Nazwa:
+                    return False
+        return True
 
     def __str__(self):
         return self.Nazwa + '(' + ','.join(str(x) for x in self.Argumenty) + ')'
@@ -26,8 +45,10 @@ class Literal:
         nazwa = ('' if self.Bool else '-') + str(self.Predykat)
         return nazwa
 
-    def __bool__(self):
-        return self.Bool
+    def rezolucjowalny(self, inny_literal):
+        if self.Bool == inny_literal.Bool:
+            return False
+        return self.Predykat.rezolucjowalny(inny_literal.Predykat)
 
     __repr__ = __str__
 
--- maszynka.py
+++ maszynka.py
@@ -1,7 +1,9 @@
+import copy
+
 from Klasy import Argument, Predykat, Literal, Klauzula
 
 tekst = open('fakty.txt', 'r').read()
-zdania = [x.strip() for x in tekst.split(',')]
+zdania = tekst.split('\n')
 
 priorytety = {
     '-': 1,
@@ -47,6 +49,8 @@ def parsuj(zdanie):
             return lista
 
 
+# TODO: dla ostatniego elementu przed parsowaniem na klauzule zaznaczyc ze powstale klauzule to TEZY
+
 def parsuj_predykat(predykat):
     nazwa, args = predykat.split('(')
     argumenty = [Argument(x.strip()) for x in args[:-1].split(',')]
@@ -64,6 +68,7 @@ def stworz_klauzule(zdanie):
     if zdanie[1] in priorytety:  # klauzula, wiec trzeba wyluskac literaly
         klauzula1 = stworz_klauzule(zdanie[0])
         klauzula2 = stworz_klauzule(zdanie[2])
+        # TODO: if exisct add klauzula 3 and 4 and...
         return klauzula1.dodaj(klauzula2)
     else:
         literal = [stworz_literal(zdanie)]
@@ -78,6 +83,107 @@ print('pogrupowane')
 print(zdania)
 
 lista_klauzul = [stworz_klauzule(zdanie) for zdanie in zdania]
+#TODO: doadc do listy klauzul LISTE KLAUZUL Z TEZY
 print(lista_klauzul)
 
+# instnieje_rezolucja = True
+#
+# while instnieje_rezolucja:
+#     istnieje_rezolucja = False
+
+# def sprawdz_rezolucje:
+#     pass
+tezy = lista_klauzul[-1]
+roboczy = []
+
+
+def rezolucja_roboczego(lista_klauzul, roboczy_akt, roboczy):
+    if roboczy_akt is None:
+        return False
+    else:
+        for klauzula in lista_klauzul:
+            roboczy += rezolucja(klauzula, roboczy_akt)
+            roboczy_akt = roboczy[-1]
+            lista_klauzul += roboczy_akt
+            if rezolucja_roboczego(lista_klauzul, roboczy_akt, roboczy):
+                break
+
+
+# output = da_sie_rezolucje(lista_klauzul[0].Literaly[0], lista_klauzul[1].Literaly[0])
+# print(output)
+# quit()
+# lista_rezolucji = []
+
+# def zaszlaRezolucja(gornaKlauzula, dolnaKlauzula):
+#     pass
+
+
+def rezolucja(klauzula, teza):
+    gornaKlauzula = teza
+    dolnaKlauzula = klauzula
+    nowa_gorna_klauzula = copy.deepcopy(gornaKlauzula)
+    nowa_dolna_klauzula = copy.deepcopy(dolnaKlauzula)
+    lista_unifikacji = []
+    unifikacja_gorna = []
+    unifikacja_dolna = []
+    rezolucjowalny = False
+    for literal_gorny in gornaKlauzula.Literaly:
+        for literal_dolny in dolnaKlauzula.Literaly:
+            if literal_gorny.rezolucjowalny(literal_dolny):
+                for index, arg_gorny in enumerate(literal_gorny.Predykat.Argumenty):
+                    arg_dolny = literal_dolny.Predykat.Argumenty[index]
+                    if arg_gorny.Nazwa != arg_dolny.Nazwa:
+                        lista_unifikacji.extend([arg_gorny, arg_dolny])
+                for i in range(0, len(lista_unifikacji), 2):
+                    pierwszy = lista_unifikacji[i]
+                    drugi = lista_unifikacji[i + 1]
+                    if not pierwszy.stala() and drugi.stala():
+                        # for lit in nowa_gorna_klauzula.Literaly:
+                        #     for arg in lit.Predykat.Argumenty:
+                        #         if arg.Nazwa == pierwszy.Nazwa:
+                        unifikacja_gorna.extend([pierwszy.Nazwa, drugi.Nazwa])
+
+                    if not pierwszy.stala() and not drugi.stala() and pierwszy.Nazwa != drugi.Nazwa:
+                        # for lit in nowa_gorna_klauzula.Literaly:
+                        #     for arg in lit.Predykat.Argumenty:
+                        #         if arg.Nazwa == pierwszy.Nazwa:
+                        unifikacja_gorna.extend([pierwszy.Nazwa, drugi.Nazwa])
+
+                    if pierwszy.stala() and not drugi.stala():
+                        unifikacja_dolna.extend([drugi.Nazwa, pierwszy.Nazwa])
+
+                for lit in nowa_gorna_klauzula.Literaly:
+                    for arg in lit.Predykat.Argumenty:
+                        for i in range(0, len(unifikacja_gorna), 2):
+                            if arg.Nazwa == unifikacja_gorna[i]:
+                                arg.Nazwa = unifikacja_gorna[i + 1]
+                                break
+
+                for lit in nowa_dolna_klauzula.Literaly:
+                    for arg in lit.Predykat.Argumenty:
+                        for i in range(0, len(unifikacja_dolna), 2):
+                            if arg.Nazwa == unifikacja_dolna[i]:
+                                arg.Nazwa = unifikacja_dolna[i + 1]
+                                break
+
+
+tezy = [lista_klauzul[-1]]
+
+
+def wnioskuj(lista_klauzul, roboczy):
+    for klauzula in lista_klauzul:
+        for teza in tezy:
+            try:
+                roboczy += rezolucja(klauzula, teza)
+            except TypeError:
+                continue
+            lista_klauzul += roboczy
+            roboczy_akt = roboczy[-1]
+            if rezolucja_roboczego(lista_klauzul, roboczy_akt, roboczy):
+                break
+
+
+wnioskuj(lista_klauzul, roboczy)
+
+# TODO: odzielna lista tez
 # TODO: Clauses (for now only 2 argument clauses) are parsed properly, now we have to implement algorithm.
