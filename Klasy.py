36e037a4c7deae616792d95178c537cd20227b37
Radoslaw Komorowski <283680@pw.edu.pl>
2019-01-19 02:54:09 +0100

    abandon work on implication and '&' operator parsing since it is complicated and the assignment does not require it; add classes and parsing text into those.

---
+++ Klasy.py
@@ -0,0 +1,40 @@
+class Argument:
+    def __init__(self, nazwa):
+        self.Nazwa = nazwa
+
+    def __str__(self):
+        return self.Nazwa
+
+    __repr__ = __str__  # dont do that often, repr should return Argument(nazwa) rather than nazwa itself..
+
+
+class Predykat:
+    def __init__(self, nazwa, argumenty):
+        self.Nazwa = nazwa
+        self.Argumenty = argumenty
+
+    def __str__(self):
+        return self.Nazwa + '(' + ','.join(str(x) for x in self.Argumenty) + ')'
+
+
+class Literal:
+    def __init__(self, booly, predykat):
+        self.Bool = booly
+        self.Predykat = predykat
+
+    def __str__(self):
+        nazwa = ('' if self.Bool else '-') + str(self.Predykat)
+        return nazwa
+
+    def __bool__(self):
+        return self.Bool
+
+
+class Klauzula:
+    def __init__(self, literaly):
+        self.Literaly = literaly
+
+    def __str__(self):
+        return '|'.join(str(x) for x in self.Literaly)
+
+    __repr__ = __str__
--- maszynka.py
+++ maszynka.py
@@ -1,7 +1,6 @@
-fakty = "-((A()=B()))"
-#wniosek = "C()"
-tekst = fakty #+ ', -' + wniosek
-#tekst = open('fakty.txt', 'r').read()
+from Klasy import Argument, Predykat, Literal, Klauzula
+
+tekst = open('fakty.txt', 'r').read()
 zdania = [x.strip() for x in tekst.split(',')]
 
 priorytety = {
@@ -13,7 +12,8 @@ priorytety = {
 }
 
 
-def szukajNajwPrio(zdanie):
+# szuka operatora o najwyzszym priorytecie (uwzgedniajac pierwszenstwo nawiasu)
+def szukaj_najw_priorytet(zdanie):
     poziom_nawiasow = 0
     najwyzszy_pri = -1
     dzialanie = None
@@ -28,9 +28,9 @@ def szukajNajwPrio(zdanie):
     return najwyzszy_pri, dzialanie
 
 
+# tworzy listę list zachowując hierarchię wg. priorytetów operatorów
 def parsuj(zdanie):
-    # musimy podzielic zdanie ze wzgledu na priorytety kwantyfikatorow
-    najwyzszy_pri, dzialanie = szukajNajwPrio(zdanie)
+    najwyzszy_pri, dzialanie = szukaj_najw_priorytet(zdanie)
     if najwyzszy_pri == -1:
         if zdanie[0] == '(':
             return parsuj(zdanie[1:-1])
@@ -40,75 +40,33 @@ def parsuj(zdanie):
             lista = [dzialanie, parsuj(zdanie[1:])]  # TODO: zmienic strip na lepszy zeby bralo stringa po nawiasie
             return lista
         else:
-            zdania = zdanie.split(dzialanie)
-            lst1 = parsuj(zdania[0])
-            lst2 = parsuj(zdania[1])
+            operandy = zdanie.split(dzialanie)
+            lst1 = parsuj(operandy[0])
+            lst2 = parsuj(operandy[1])
             lista = [lst1, dzialanie, lst2]
             return lista
 
 
-def zamienRownowaznosc(zdanie):
-    zdanko = zdanie
-    if zdanko[0] == '-':
-        zdanko[1] = zamienRownowaznosc(zdanko[1])  # TODEBUG: lista lub nie lista to debug.
-    elif zdanko[1] in priorytety:
-        zdanko[0] = zamienRownowaznosc(zdanko[0])
-        zdanko[2] = zamienRownowaznosc(zdanko[2])
-        if zdanko[1] == '=':
-            lewa_implikacja = [zdanko[0], '>', zdanko[2]]
-            prawa_implikacja = [zdanko[2], '>', zdanko[0]]
-            zdanko = [lewa_implikacja, '&', prawa_implikacja]
-
-    return zdanko
+def parsuj_predykat(predykat):
+    nazwa, args = predykat.split('(')
+    argumenty = [Argument(x.strip()) for x in args[:-1].split(',')]
+    return Predykat(nazwa, argumenty)
 
 
-def nagacja(param):
-    if param[0] == '-':
-        return param[1]
+def stworz_literal(zdanie):
+    bool_val = False if zdanie[0] == '-' else True
+    predykat = zdanie if bool_val else zdanie[1]
+    predykat = parsuj_predykat(predykat)
+    return Literal(bool_val, predykat)
+
+
+def stworz_klauzule(zdanie):
+    if zdanie[1] in priorytety:  # klauzula, wiec trzeba wyluskac literaly
+        klauzule = [stworz_klauzule(zdanie[0]), stworz_klauzule(zdanie[2])]
+        return klauzule
     else:
-        return ['-', param]
-
-
-def zamienImplikacje(zdanie):
-    zdanko = zdanie
-    if zdanko[0] == '-':
-        zdanko[1] = zamienImplikacje(zdanko[1])  # TODEBUG: lista lub nie lista to debug.
-    elif zdanko[1] in priorytety:
-        zdanko[0] = zamienImplikacje(zdanko[0])
-        zdanko[2] = zamienImplikacje(zdanko[2])
-        if zdanko[1] == '>':
-            zdanko = [nagacja(zdanko[0]), '|', zdanko[2]]
-
-    return zdanko
-
-
-def redukcjaMinusow(zdanie):
-    zdanko = zdanie
-    if zdanko[0] == '-':
-        if isinstance(zdanko[1], str):  # literal, jesli po minusie nie ma listy tylko odrazu strink
-            return zdanko
-        elif zdanko[1][0] == '-':  # podwojne przeczenie, tu redukcja
-            zdanko[1][1] = redukcjaMinusow(zdanko[1][1])
-        else:  # elif zdanko [1] in priori:
-            zdanko[1][0] = redukcjaMinusow(nagacja(zdanko[1][0]))   #negujemu bo przed nawiasem minus (weszlismy do pierwszego ifa)
-            zdanko[1][2] = redukcjaMinusow(nagacja(zdanko[1][2]))
-            if zdanko[1][1] == '&':
-                zdanko[1][1] = '|'
-            elif zdanko[1][1] == '|':
-                zdanko[1][1] = '&'
-            return zdanko[1]
-    elif zdanko[1] in priorytety:
-        zdanko[0] = redukcjaMinusow(zdanko[0])
-        zdanko[2] = redukcjaMinusow(zdanko[2])
-    return zdanko
-    #     zdanko[1] = zamienImplikacje(zdanko[1])  # TODEBUG: lista lub nie lista to debug.
-    # elif zdanko[1] in priorytety:
-    #     zdanko[0] = zamienImplikacje(zdanko[0])
-    #     zdanko[2] = zamienImplikacje(zdanko[2])
-    #     if zdanko[1] == '>':
-    #         zdanko = [nagacja(zdanko[0]), '|', zdanko[2]]
-    #
-    # return zdanko
+        literaly = [stworz_literal(zdanie)]
+        return Klauzula(literaly)
 
 
 print('oryginalne')
@@ -118,78 +76,126 @@ zdania = [parsuj(zdanie) for zdanie in zdania]
 print('pogrupowane')
 print(zdania)
 
-zdania = [zamienRownowaznosc(zdanie) for zdanie in zdania]
-print('zamieniona rownowaznosc')
-print(zdania)
+lista_klauzul = [stworz_klauzule(zdanie) for zdanie in zdania]
+print(lista_klauzul)
 
-zdania = [zamienImplikacje(zdanie) for zdanie in zdania]
-print('zamieniona implik')
-print(zdania)
-
-zdania = [redukcjaMinusow(zdanie) for zdanie in zdania]
-print('zredukowane minusy')
-print(zdania)
-
-
-def rozbijAlternatywe2(zdanie):
-    pass
-
-
-def rozbijAlternatywe(zdanie):
-    zdanko = zdanie
-    if zdanko[0][1] in priorytety:          # [0] is left operand, [1] is sign [2] is right operand
-        zdanko = rozbijAlternatywe2(zdanko)
-        if zdanko[1] == '&':
-            return zdanko
-    if zdanko[2][1] in priorytety:
-        zdanko = rozbijAlternatywe2(zdanko)
-        return zdanko
-    return [zdanko[0], '|', zdanko[1]]
-
-
-
-def koniunkcjaAlternatyw(zdanie):
-    zdanko = zdanie
-    znak = zdanko[1]
-    if znak in priorytety:
-        if znak == '|':
-            zdanko = rozbijAlternatywe(zdanko)
-        elif znak == '&':
-            zdanko[0] = koniunkcjaAlternatyw(zdanko[0])
-            zdanko[0] = koniunkcjaAlternatyw(zdanko[0])
-        else:
-            raise ValueError('Tu nie moze byc innego znaku, a jak jest to blad parsowania')
-    return zdanko
-
-
-
-zdania = [koniunkcjaAlternatyw(zdanie) for zdanie in zdania]
-print('na koniuncje alternatyw')
-print(zdania)
-
-quit()
-
-
-
-
-def scan(_list, l=-1, i=-1):
-    if isinstance(_list, list):
-        print('list {}:{}'.format((l, i), _list))
-        l += 1
-        for i, item in enumerate(_list):
-            scan(item, l, i)
-
-
-scan(zdania)
-
-# for zdanie in lista_zdan:
-#     if len(zdanie) > 0:
-#         lista_klauzul.append(zamienRownowaznosc(zdanie))
+# zdania = [zamienRownowaznosc(zdanie) for zdanie in zdania]
+# print('zamieniona rownowaznosc')
+# print(zdania)
 #
-# lista_klauzul2 = []
+# zdania = [zamienImplikacje(zdanie) for zdanie in zdania]
+# print('zamieniona implik')
+# print(zdania)
+
+# zdania = [redukcjaMinusow(zdanie) for zdanie in zdania]
+# print('zredukowane minusy')
+# print(zdania)
+
+
+# def rozbijAlternatywe2(zdanie):
+#     pass
 #
-# for zdanie in lista_klauzul:
-#     if len(zdanie) > 0:
-#         lista_klauzul2.append(zamienImplikacje(zdanie))
 #
-# print(lista_klauzul2)
+# def rozbijAlternatywe(zdanie):
+#     zdanko = zdanie
+#     if zdanko[0][1] in priorytety:          # [0] is left operand, [1] is sign [2] is right operand
+#         zdanko = rozbijAlternatywe2(zdanko)
+#         if zdanko[1] == '&':
+#             return zdanko
+#     if zdanko[2][1] in priorytety:
+#         zdanko = rozbijAlternatywe2(zdanko)
+#         return zdanko
+#     return [zdanko[0], '|', zdanko[1]]
+
+
+# def koniunkcjaAlternatyw(zdanie):
+#     zdanko = zdanie
+#     znak = zdanko[1]
+#     if znak in priorytety:
+#         if znak == '|':
+#             zdanko = rozbijAlternatywe(zdanko)
+#         elif znak == '&':
+#             zdanko[0] = koniunkcjaAlternatyw(zdanko[0])
+#             zdanko[0] = koniunkcjaAlternatyw(zdanko[0])
+#         else:
+#             raise ValueError('Tu nie moze byc innego znaku, a jak jest to blad parsowania')
+#     return zdanko
+
+
+# zdania = [koniunkcjaAlternatyw(zdanie) for zdanie in zdania]
+# print('na koniuncje alternatyw')
+# print(zdania)
+
+
+# def zamienRownowaznosc(zdanie):
+#     zdanko = zdanie
+#     if zdanko[0] == '-':
+#         zdanko[1] = zamienRownowaznosc(zdanko[1])  # TODEBUG: lista lub nie lista to debug.
+#     elif zdanko[1] in priorytety:
+#         zdanko[0] = zamienRownowaznosc(zdanko[0])
+#         zdanko[2] = zamienRownowaznosc(zdanko[2])
+#         if zdanko[1] == '=':
+#             lewa_implikacja = [zdanko[0], '>', zdanko[2]]
+#             prawa_implikacja = [zdanko[2], '>', zdanko[0]]
+#             zdanko = [lewa_implikacja, '&', prawa_implikacja]
+#
+#     return zdanko
+# def nagacja(param):
+#     if param[0] == '-':
+#         return param[1]
+#     else:
+#         return ['-', param]
+#
+#
+# def zamienImplikacje(zdanie):
+#     zdanko = zdanie
+#     if zdanko[0] == '-':
+#         zdanko[1] = zamienImplikacje(zdanko[1])  # TODEBUG: lista lub nie lista to debug.
+#     elif zdanko[1] in priorytety:
+#         zdanko[0] = zamienImplikacje(zdanko[0])
+#         zdanko[2] = zamienImplikacje(zdanko[2])
+#         if zdanko[1] == '>':
+#             zdanko = [nagacja(zdanko[0]), '|', zdanko[2]]
+#
+#     return zdanko
+# def redukcjaMinusow(zdanie):
+#     zdanko = zdanie
+#     if zdanko[0] == '-':
+#         if isinstance(zdanko[1], str):  # literal, jesli po minusie nie ma listy tylko odrazu strink
+#             return zdanko
+#         elif zdanko[1][0] == '-':  # podwojne przeczenie, tu redukcja
+#             zdanko[1][1] = redukcjaMinusow(zdanko[1][1])
+#         else:  # elif zdanko [1] in priori:
+#             zdanko[1][0] = redukcjaMinusow(nagacja(zdanko[1][0]))   #negujemu bo przed nawiasem minus (weszlismy do pierwszego ifa)
+#             zdanko[1][2] = redukcjaMinusow(nagacja(zdanko[1][2]))
+#             if zdanko[1][1] == '&':
+#                 zdanko[1][1] = '|'
+#             elif zdanko[1][1] == '|':
+#                 zdanko[1][1] = '&'
+#             return zdanko[1]
+#     elif zdanko[1] in priorytety:
+#         zdanko[0] = redukcjaMinusow(zdanko[0])
+#         zdanko[2] = redukcjaMinusow(zdanko[2])
+#     return zdanko
+# def alternNaKlauz(zdanie):
+#     zdanko = zdanie
+#     znak = zdanko[1]
+#     if znak in priorytety:
+#         # TODO: usunac
+#         if znak != '|':
+#             raise RuntimeError("TU POWINNA BYC ALTERNATYWA NIE MA INNEJ OPCJI")
+#         klauzula = [alternNaKlauz(zdanko[0]), alternNaKlauz(zdanko[2])]
+#         return klauzula
+#     else:
+#
+#
+#
+# def stworzKlauzule(zdanie):
+#     zdanko = zdanie
+#     znak = zdanko[1]
+#     if znak == '&':
+#         klauzule = [stworzKlauzule(zdanko[0]), stworzKlauzule(zdanko[2])]
+#         return klauzule
+#
+#     klauzule = [alternNaKlauz(zdanko)]
+#     return klauzule
